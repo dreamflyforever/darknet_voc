5eaa80531ba7fdbb85908dd7c5c06898aa6cda55
dreamflyforever <1150660744@qq.com>
2022-12-16 15:57:14 +0800

    train VOC2012 dataset
    Signed-off-by: dreamflyforever <1150660744@qq.com>

--- scripts/voc_label.py
+++ scripts/voc_label.py
@@ -4,7 +4,9 @@ import os
 from os import listdir, getcwd
 from os.path import join
 
-sets=[('2012', 'train'), ('2012', 'val'), ('2007', 'train'), ('2007', 'val'), ('2007', 'test')]
+dataset = '/Users/jim/workspace/data/VOCdevkit'
+
+sets=[('2012', 'train'), ('2012', 'val')]
 
 classes = ["aeroplane", "bicycle", "bird", "boat", "bottle", "bus", "car", "cat", "chair", "cow", "diningtable", "dog", "horse", "motorbike", "person", "pottedplant", "sheep", "sofa", "train", "tvmonitor"]
 
@@ -23,8 +25,8 @@ def convert(size, box):
     return (x,y,w,h)
 
 def convert_annotation(year, image_id):
-    in_file = open('VOCdevkit/VOC%s/Annotations/%s.xml'%(year, image_id))
-    out_file = open('VOCdevkit/VOC%s/labels/%s.txt'%(year, image_id), 'w')
+    in_file = open('%s/VOC%s/Annotations/%s.xml'%(dataset, year, image_id))
+    out_file = open('%s/VOC%s/labels/%s.txt'%(dataset, year, image_id), 'w')
     tree=ET.parse(in_file)
     root = tree.getroot()
     size = root.find('size')
@@ -43,17 +45,14 @@ def convert_annotation(year, image_id):
         out_file.write(str(cls_id) + " " + " ".join([str(a) for a in bb]) + '\n')
 
 wd = getcwd()
-
 for year, image_set in sets:
-    if not os.path.exists('VOCdevkit/VOC%s/labels/'%(year)):
-        os.makedirs('VOCdevkit/VOC%s/labels/'%(year))
-    image_ids = open('VOCdevkit/VOC%s/ImageSets/Main/%s.txt'%(year, image_set)).read().strip().split()
+    if not os.path.exists('%s/VOC%s/labels/'%(dataset, year)):
+        os.makedirs('%s/VOC%s/labels/'%(dataset, year))
+    image_ids = open('%s/VOC%s/ImageSets/Main/%s.txt'%(dataset, year, image_set)).read().strip().split()
     list_file = open('%s_%s.txt'%(year, image_set), 'w')
     for image_id in image_ids:
-        list_file.write('%s/VOCdevkit/VOC%s/JPEGImages/%s.jpg\n'%(wd, year, image_id))
+        list_file.write('%s/VOC%s/JPEGImages/%s.jpg\n'%(dataset, year, image_id))
         convert_annotation(year, image_id)
     list_file.close()
 
-os.system("cat 2007_train.txt 2007_val.txt 2012_train.txt 2012_val.txt > train.txt")
-os.system("cat 2007_train.txt 2007_val.txt 2007_test.txt 2012_train.txt 2012_val.txt > train.all.txt")
-
+os.system("cat 2012_train.txt 2012_val.txt > train.all.txt")
